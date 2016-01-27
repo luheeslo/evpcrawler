c78d3ec9d7492c03592596a311b34c8270f0dce9
lhel <luishenriqueestrela@gmail.com>
2016-01-26 21:12:26 -0300

    listagem de matérias e cursos

--- crawler.py
+++ crawler.py
@@ -1,4 +1,3 @@
-import os
 import requests
 from lxml import html
 import json
@@ -6,27 +5,43 @@ import json
 url = 'https://www.euvoupassar.com.br'
 login_url = 'https://www.euvoupassar.com.br/login/index/autenticar'
 cursos_url = 'https://www.euvoupassar.com.br/cursos/indice'
-auth = {
-    'email': os.environ.get('EVP_EMAIL'),
-    'senha': os.environ.get('EVP_PASSWORD')
-}
 
 
-def main():
+def login(email, senha):
     session = requests.session()
+    auth = {
+        'email': email,
+        'senha': senha
+    }
     response = session.post(login_url, data=auth)
     json_response = json.loads(response.content.decode('utf-8'))
     mensagem = json_response['mensagem']
     if mensagem == 'Acesso liberado. Aguarde...':
         print(mensagem)
-        page = session.get(cursos_url, allow_redirects=False)
+        return EvpCrawler(session)
+    else:
+        print(mensagem)
+
+
+class EvpCrawler:
+
+    def __init__(self, session):
+        self._session = session
+
+    def materias(self):
+        page = self._session.get(cursos_url, allow_redirects=False)
         tree = html.fromstring(page.content)
         lis_query = '/html/body/div[1]/section/div/div[2]/div/section/ul/li'
         materias = tree.xpath(lis_query)
         for materia in materias:
-            print(materia.xpath('span/text()'))
-    else:
-        print(mensagem)
+            nome_materia = materia.xpath('span/text()')[0]
+            yield nome_materia, materia
 
-if __name__ == '__main__':
-    main()
+    def cursos(self, nome_materia):
+        for materia in self.materias():
+            if materia[0] == nome_materia:
+                cursos = materia[1].xpath('ul/li')
+                for curso in cursos:
+                    nome_curso = curso.xpath('span/text()')[0]
+                    link_curso = curso.xpath('span/a/@href')[0]
+                    yield nome_curso, link_curso

3b40fc6844656aaec7253d1c30e72fe1b0aac012
lhel <luishenriqueestrela@gmail.com>
2016-01-31 18:35:49 -0300

    add listagem de aulas

--- crawler.py
+++ crawler.py
@@ -31,11 +31,13 @@ class EvpCrawler:
     def materias(self):
         page = self._session.get(cursos_url, allow_redirects=False)
         tree = html.fromstring(page.content)
+
         lis_query = '/html/body/div[1]/section/div/div[2]/div/section/ul/li'
+
         materias = tree.xpath(lis_query)
-        for materia in materias:
-            nome_materia = materia.xpath('span/text()')[0]
-            yield nome_materia, materia
+        for li_materia in materias:
+            nome_materia = li_materia.xpath('span/text()')[0]
+            yield nome_materia, li_materia
 
     def cursos(self, nome_materia):
         for materia in self.materias():
@@ -43,5 +45,22 @@ class EvpCrawler:
                 cursos = materia[1].xpath('ul/li')
                 for curso in cursos:
                     nome_curso = curso.xpath('span/text()')[0]
-                    link_curso = curso.xpath('span/a/@href')[0]
-                    yield nome_curso, link_curso
+                    url_aulas = curso.xpath('span/a/@href')[0]
+                    yield nome_curso, url_aulas
+
+    def aulas(self, url_aulas):
+        page = self._session.get(url_aulas, allow_redirects=False)
+        tree = html.fromstring(page.content)
+
+        aulas = tree.xpath('//div[@class="bloco-branco"]/ul/li')
+        for aula in aulas:
+            titulo_aula = aula.xpath('span/a/text()')[0]
+            data_aula = aula.xpath('div/div/@data-aula')[0]
+            mp4 = aula.xpath('div/div/a[@class="mp4 baixarMP4 "]')
+            pdf = aula.xpath('div/div/a[@class="pdf baixarPDF "]')
+            yield (
+                titulo_aula,
+                data_aula,
+                True if mp4 else False,
+                True if pdf else False
+            )

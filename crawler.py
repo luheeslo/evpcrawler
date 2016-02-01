a07a692288603c7cc13cf82ed2222b7a677883e1
lhel <luishenriqueestrela@gmail.com>
2016-02-01 00:57:48 -0300

    add listagem de aulas, download de aulas

--- crawler.py
+++ crawler.py
@@ -1,10 +1,12 @@
+import os
 import requests
 from lxml import html
 import json
 
-url = 'https://www.euvoupassar.com.br'
-login_url = 'https://www.euvoupassar.com.br/login/index/autenticar'
-cursos_url = 'https://www.euvoupassar.com.br/cursos/indice'
+URL = 'https://www.euvoupassar.com.br'
+LOGIN_URL = 'https://www.euvoupassar.com.br/login/index/autenticar'
+CURSOS_URL = 'https://www.euvoupassar.com.br/cursos/indice'
+DOWNLOAD_URL = 'https://www.euvoupassar.com.br/application/download/'
 
 
 def login(email, senha):
@@ -13,7 +15,7 @@ def login(email, senha):
         'email': email,
         'senha': senha
     }
-    response = session.post(login_url, data=auth)
+    response = session.post(LOGIN_URL, data=auth)
     json_response = json.loads(response.content.decode('utf-8'))
     mensagem = json_response['mensagem']
     if mensagem == 'Acesso liberado. Aguarde...':
@@ -29,7 +31,7 @@ class EvpCrawler:
         self._session = session
 
     def materias(self):
-        page = self._session.get(cursos_url, allow_redirects=False)
+        page = self._session.get(CURSOS_URL, allow_redirects=False)
         tree = html.fromstring(page.content)
 
         lis_query = '/html/body/div[1]/section/div/div[2]/div/section/ul/li'
@@ -45,11 +47,11 @@ class EvpCrawler:
                 cursos = materia[1].xpath('ul/li')
                 for curso in cursos:
                     nome_curso = curso.xpath('span/text()')[0]
-                    url_aulas = curso.xpath('span/a/@href')[0]
-                    yield nome_curso, url_aulas
+                    aulas_url = curso.xpath('span/a/@href')[0]
+                    yield nome_curso, aulas_url
 
-    def aulas(self, url_aulas):
-        page = self._session.get(url_aulas, allow_redirects=False)
+    def aulas(self, aulas_url):
+        page = self._session.get(aulas_url, allow_redirects=False)
         tree = html.fromstring(page.content)
 
         aulas = tree.xpath('//div[@class="bloco-branco"]/ul/li')
@@ -61,6 +63,25 @@ class EvpCrawler:
             yield (
                 titulo_aula,
                 data_aula,
-                True if mp4 else False,
-                True if pdf else False
+                'mp4' if mp4 else '',
+                'pdf' if pdf else ''
             )
+
+    def baixarAula(self, data_aula, mp4, pdf, path=''):
+        for type in (mp4, pdf):
+            if type:
+                response = self._session.post(DOWNLOAD_URL + type,
+                                              data={'id': data_aula})
+                json_response = json.loads(response.content.decode('utf-8'))
+                url = json_response['url']
+                response = self._session.get(json_response['url'], stream=True)
+                if response.status_code == 200:
+                    nome_arquivo = url.split('/').pop()
+                    path = os.path.join(path, nome_arquivo)
+                    tamanho_arquivo = int(response.headers['content-length'])
+                    bytes_enviados = 0
+                    with open(path, 'wb') as f:
+                        for chunk in response.iter_content(1024):
+                            f.write(chunk)
+                            bytes_enviados += len(chunk)
+                            yield bytes_enviados, tamanho_arquivo
